180b6992817e5c8b26d7b5f51d0d8e0f41ae49bb
Kévin Mathieu <kevin.mathieu@insidegroup.fr>
2017-09-11 20:10:36 +0200

    Typo in scripts name Add camera script

---
+++ script_time_camera_rdc.lua
@@ -0,0 +1,42 @@
+-----------------------------------------
+-- Gestion de la caméra rdc
+-----------------------------------------
+
+-- Variables
+
+local calendrier = 'Caméra rdc'
+
+cameras = {
+	'cam_rdc'
+}
+
+commandArray = {}
+
+function onOffCamera(command)
+	for key, camera in pairs(cameras) do
+		commandArray[camera] = command
+	end
+end
+
+-- Recupere les minutes
+time = os.time()
+minutes = tonumber(os.date('%M',time))
+
+if (minutes % 5 == 0) then
+	-- Sécurité off
+	if (otherdevices['Sécurité'] == 'Off') then
+		print('Off camera rdc')
+		onOffCamera('off')
+	-- Calendrier
+	else
+		if (otherdevices[calendrier] == 'On') then
+			print('On camera rdc calendrier')
+			onOffCamera('on')
+		else
+			print('Off camera rdc calendrier')
+			onOffCamera('off')
+		end
+	end
+end
+
+return commandArray
