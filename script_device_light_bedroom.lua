0b4bc0ff8f0f30bd130c19a8997c193edb77a648
k20human <kmat.null@gmail.com>
2017-10-16 15:08:55 +0200

    Try new light management

--- script_device_light_bedroom.lua
+++ script_device_light_bedroom.lua
@@ -4,10 +4,11 @@ interupteur = 'Lampes chambre parents'
 lampe1 = 'Lampe chambre parents 1'
 lampe2 = 'Lampe chambre parents 2'
 
+lampe1Idx = '79'
+
 
 if devicechanged[interupteur] and otherdevices[lampe1] == 'Off' then
-    commandArray[lampe1] = 'Set Level 80'
-    commandArray[lampe2] = 'Set Level 80'
+    commandArray['OpenURL']='http://192.168.1.14:8080/json.htm?type=command&param=setcolbrightnessvalue&idx=' .. lampe1Idx .. '&hex=FFFFFF&brightness=100&iswhite=false'
 elseif devicechanged[interupteur] and otherdevices[lampe1] ~= 'Off' then
     commandArray[lampe1] = 'Off'
     commandArray[lampe2] = 'Off'
