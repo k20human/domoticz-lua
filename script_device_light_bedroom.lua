fb25c174ccc23cd4cf325050243f58b2387ea4b4
k20human <kmat.null@gmail.com>
2017-10-31 14:55:14 +0100

    Rework heating management Add new light in parents bedroom

--- script_device_light_bedroom.lua
+++ script_device_light_bedroom.lua
@@ -1,14 +1,16 @@
 commandArray = {}
 
-interupteur = 'Lampes chambre parents'
+interupteur = 'Interrupteur lampes chambre parents'
 lampe1 = 'Lampe chambre parents 1'
 lampe2 = 'Lampe chambre parents 2'
 
 lampe1Idx = '79'
+lampe2Idx = '95'
 
 
 if devicechanged[interupteur] and otherdevices[lampe1] == 'Off' then
     commandArray['OpenURL']='http://192.168.1.14:8080/json.htm?type=command&param=setcolbrightnessvalue&idx=' .. lampe1Idx .. '&hex=FFFFFF&brightness=100&iswhite=false'
+    commandArray['OpenURL']='http://192.168.1.14:8080/json.htm?type=command&param=setcolbrightnessvalue&idx=' .. lampe2Idx .. '&hex=FFFFFF&brightness=100&iswhite=false'
 elseif devicechanged[interupteur] and otherdevices[lampe1] ~= 'Off' then
     commandArray[lampe1] = 'Off'
     commandArray[lampe2] = 'Off'
---
+++ script_time_radiateurs.lua
@@ -0,0 +1,72 @@
+-----------------------------------------
+-- Gestion des radiateurs
+-----------------------------------------
+
+package.path = package.path .. ';' .. '/home/k20/domoticz/scripts/lua/?.lua'
+Library = require('Library')
+
+-- Variables
+
+local hysteresis = 0.5 -- Valeur seuil pour éviter que le relai ne cesse de commuter dans les 2 sens
+local tempVacs = 10
+
+local sonde = 'Température '
+local thermostat = 'Thermostat '
+local calendrier = 'Calendrier chauffage '
+local management = 'Gestion chauffage '
+
+zones = {
+	['chambre Théa'] = {'Radiateur chambre Théa'},
+	['salle de bain'] = {'Radiateur salle de bain'},
+	['rdc'] = {
+		'Radiateur salon',
+		'Radiateur salle à manger'
+	},
+}
+
+commandArray = {}
+
+-- Recupere les minutes
+minutes = Library.getCurrentMinutes()
+
+if (minutes % 5 == 0) then
+	-- On parcourt les zones
+	for zone, radiateurs in pairs(zones) do
+        print('Chauffage - Zone ' .. zone)
+
+		local temperature = tonumber(Library.split(otherdevices[sonde + zone], ";")[0])
+		local thermostatValue = tonumber(otherdevices[thermostat + zone])
+
+		-- Mode vacances
+		if (otherdevices[management + zone] == 'Vacances') then
+			if (temperature <= (tempVacs - hysteresis) ) then
+				Library.onOffDevices(radiateurs, 'On', 'vacances')
+			elseif (temperature >= (tempVacs + hysteresis) ) then
+				Library.onOffDevices(radiateurs, 'Off', 'vacances')
+			end
+		-- Mode forcé
+		elseif (otherdevices[management + zone] == 'Mode forcé') then
+			if (temperature <= (thermostatValue - hysteresis) ) then
+				Library.onOffDevices(radiateurs, 'On', 'mode forcé')
+			elseif (temperature >= (thermostatValue + hysteresis) ) then
+				Library.onOffDevices(radiateurs, 'Off', 'mode forcé')
+			end
+		-- Calendrier
+		elseif (otherdevices[management + zone] == 'On') then
+			if (otherdevices[calendrier + zone] == 'On') then
+				if (temperature <= (thermostatValue - hysteresis) ) then
+					Library.onOffDevices(radiateurs, 'On', 'calendrier')
+				elseif (temperature >= (thermostatValue + hysteresis) ) then
+					Library.onOffDevices(radiateurs, 'Off', 'calendrier')
+				end
+			else
+				Library.onOffDevices(radiateurs, 'Off', 'calendrier fin')
+			end
+		-- Off
+		elseif (otherdevices[management + zone] == 'Off') then
+			Library.onOffDevices(radiateurs, 'Off', '')
+		end
+	end
+end
+
+return commandArray
