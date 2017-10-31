f2baf827d641f9e4c6d17c6bbaa112b9a51af056
k20human <kmat.null@gmail.com>
2017-10-31 15:06:14 +0100

    Wrong operator

--- script_time_radiateurs.lua
+++ script_time_radiateurs.lua
@@ -34,26 +34,26 @@ if (minutes % 5 == 0) then
 	for zone, radiateurs in pairs(zones) do
         print('Chauffage - Zone ' .. zone)
 
-		local temperature = tonumber(Library.split(otherdevices[sonde + zone], ";")[0])
-		local thermostatValue = tonumber(otherdevices[thermostat + zone])
+		local temperature = tonumber(Library.split(otherdevices[sonde .. zone], ";")[0])
+		local thermostatValue = tonumber(otherdevices[thermostat .. zone])
 
 		-- Mode vacances
-		if (otherdevices[management + zone] == 'Vacances') then
+		if (otherdevices[management .. zone] == 'Vacances') then
 			if (temperature <= (tempVacs - hysteresis) ) then
 				Library.onOffDevices(radiateurs, 'On', 'vacances')
 			elseif (temperature >= (tempVacs + hysteresis) ) then
 				Library.onOffDevices(radiateurs, 'Off', 'vacances')
 			end
 		-- Mode forcé
-		elseif (otherdevices[management + zone] == 'Mode forcé') then
+		elseif (otherdevices[management .. zone] == 'Mode forcé') then
 			if (temperature <= (thermostatValue - hysteresis) ) then
 				Library.onOffDevices(radiateurs, 'On', 'mode forcé')
 			elseif (temperature >= (thermostatValue + hysteresis) ) then
 				Library.onOffDevices(radiateurs, 'Off', 'mode forcé')
 			end
 		-- Calendrier
-		elseif (otherdevices[management + zone] == 'On') then
-			if (otherdevices[calendrier + zone] == 'On') then
+		elseif (otherdevices[management .. zone] == 'On') then
+			if (otherdevices[calendrier .. zone] == 'On') then
 				if (temperature <= (thermostatValue - hysteresis) ) then
 					Library.onOffDevices(radiateurs, 'On', 'calendrier')
 				elseif (temperature >= (thermostatValue + hysteresis) ) then
@@ -63,7 +63,7 @@ if (minutes % 5 == 0) then
 				Library.onOffDevices(radiateurs, 'Off', 'calendrier fin')
 			end
 		-- Off
-		elseif (otherdevices[management + zone] == 'Off') then
+		elseif (otherdevices[management .. zone] == 'Off') then
 			Library.onOffDevices(radiateurs, 'Off', '')
 		end
 	end
