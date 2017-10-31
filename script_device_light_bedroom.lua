e74fbc831f87aa8b2e0ea80b7be8cb9eae503dda
k20human <kmat.null@gmail.com>
2017-10-31 15:03:52 +0100

    Correct bedroom lights

--- script_device_light_bedroom.lua
+++ script_device_light_bedroom.lua
@@ -9,8 +9,8 @@ lampe2Idx = '95'
 
 
 if devicechanged[interupteur] and otherdevices[lampe1] == 'Off' then
-    commandArray['OpenURL']='http://192.168.1.14:8080/json.htm?type=command&param=setcolbrightnessvalue&idx=' .. lampe1Idx .. '&hex=FFFFFF&brightness=100&iswhite=false'
-    commandArray['OpenURL']='http://192.168.1.14:8080/json.htm?type=command&param=setcolbrightnessvalue&idx=' .. lampe2Idx .. '&hex=FFFFFF&brightness=100&iswhite=false'
+    commandArray[#commandArray +1] = { ['OpenURL'] = 'http://192.168.1.14:8080/json.htm?type=command&param=setcolbrightnessvalue&idx=' .. lampe1Idx .. '&hex=FFFFFF&brightness=100&iswhite=false' }
+    commandArray[#commandArray +1] = { ['OpenURL'] = 'http://192.168.1.14:8080/json.htm?type=command&param=setcolbrightnessvalue&idx=' .. lampe2Idx .. '&hex=FFFFFF&brightness=100&iswhite=false' }
 elseif devicechanged[interupteur] and otherdevices[lampe1] ~= 'Off' then
     commandArray[lampe1] = 'Off'
     commandArray[lampe2] = 'Off'
