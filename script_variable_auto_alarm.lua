9dbc371b37f15c77f61373d214785407d1b7cc54
Kévin Mathieu <kevin.mathieu@insidegroup.fr>
2017-11-18 18:28:01 +0100

    Add more log on auto alarm

--- script_variable_auto_alarm.lua
+++ script_variable_auto_alarm.lua
@@ -15,8 +15,10 @@ print(beaconHome .. ' Nut à la maison')
 
 if otherdevices[security] == 'Absence' and beaconHome >= 1 then -- switch Off Alarm because 1 beacon come back Home
     commandArray[security] = 'On'
+    print('Auto désactivation alarme')
 elseif otherdevices[security] ~= 'Absence' and beaconHome == 0 then -- switch On Alarm because all beacon are away
     commandArray[security] = 'Absence'
+    print('Auto activation alarme')
 end
 
 return commandArray
