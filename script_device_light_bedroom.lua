7a9144a1ce2340b70bbf6094009125d26e168d6b
Kévin Mathieu <kevin.mathieu@insidegroup.fr>
2017-11-25 17:32:58 +0100

    Otherdevices not work with groups

--- script_device_light_bedroom.lua
+++ script_device_light_bedroom.lua
@@ -2,10 +2,11 @@ commandArray = {}
 
 interupteur = 'Interrupteur lampes chambre parents'
 lampes = 'Lampes chambre parents'
+lampe1 = 'Lampe chambre parents 1'
 
-if devicechanged[interupteur] and otherdevices['Group:' .. lampes] == 'Off' then
+if devicechanged[interupteur] and otherdevices[lampe1] == 'Off' then
     commandArray['Group:' .. lampes] = 'On'
-elseif devicechanged[interupteur] and otherdevices['Group:' .. lampes] ~= 'Off' then
+elseif devicechanged[interupteur] and otherdevices[lampe1] ~= 'Off' then
     commandArray['Group:' .. lampes] = 'Off'
 end
 
