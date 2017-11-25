32cbc99592e57f23a84cba3112676c32e16a4030
Kévin Mathieu <kevin.mathieu@insidegroup.fr>
2017-11-25 17:28:33 +0100

    Fix

--- script_device_light_bedroom.lua
+++ script_device_light_bedroom.lua
@@ -3,9 +3,9 @@ commandArray = {}
 interupteur = 'Interrupteur lampes chambre parents'
 lampes = 'Lampes chambre parents'
 
-if devicechanged[interupteur] and otherdevices[lampe1] == 'Off' then
+if devicechanged[interupteur] and otherdevices['Group:' .. lampes] == 'Off' then
     commandArray['Group:' .. lampes] = 'On'
-elseif devicechanged[interupteur] and otherdevices[lampe1] ~= 'Off' then
+elseif devicechanged[interupteur] and otherdevices['Group:' .. lampes] ~= 'Off' then
     commandArray['Group:' .. lampes] = 'Off'
 end
 
