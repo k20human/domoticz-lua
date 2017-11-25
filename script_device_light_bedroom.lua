2d6409e6f190d65375d921b4dffcdab076436600
Kévin Mathieu <kevin.mathieu@insidegroup.fr>
2017-11-25 17:25:36 +0100

    Use group for bedroom switch

--- script_device_light_bedroom.lua
+++ script_device_light_bedroom.lua
@@ -1,19 +1,12 @@
 commandArray = {}
 
 interupteur = 'Interrupteur lampes chambre parents'
-lampe1 = 'Lampe chambre parents 1'
-lampe2 = 'Lampe chambre parents 2'
-
-lampe1Idx = '79'
-lampe2Idx = '95'
-
+lampes = 'Lampes chambre parents'
 
 if devicechanged[interupteur] and otherdevices[lampe1] == 'Off' then
-    commandArray[#commandArray +1] = { ['OpenURL'] = 'http://192.168.1.14:8080/json.htm?type=command&param=setcolbrightnessvalue&idx=' .. lampe1Idx .. '&hex=FFFFFF&brightness=100&iswhite=false' }
-    commandArray[#commandArray +1] = { ['OpenURL'] = 'http://192.168.1.14:8080/json.htm?type=command&param=setcolbrightnessvalue&idx=' .. lampe2Idx .. '&hex=FFFFFF&brightness=100&iswhite=false' }
+    commandArray['Group:' .. lampes] = 'On'
 elseif devicechanged[interupteur] and otherdevices[lampe1] ~= 'Off' then
-    commandArray[lampe1] = 'Off'
-    commandArray[lampe2] = 'Off'
+    commandArray['Group:' .. lampes] = 'Off'
 end
 
 return commandArray
