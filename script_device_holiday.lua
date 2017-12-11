328e7ed6b3b5f8c02cd22bbbac9953e1b93f8f02
Kévin Mathieu <kevin.mathieu@insidegroup.fr>
2017-12-11 09:30:03 +0100

    Wrong variable

--- script_device_holiday.lua
+++ script_device_holiday.lua
@@ -9,11 +9,11 @@ heating_management = {
 }
 
 if devicechanged[interupteur] and otherdevices[interupteur] == 'On' then
-    for deviceName, deviceValue in pairs(devicechanged) do
+    for deviceName, deviceValue in pairs(heating_management) do
         commandArray['Gestion chauffage ' .. deviceName] = 'Vacances'
     end
 elseif devicechanged[interupteur] and otherdevices[interupteur] == 'Off' then
-    for deviceName, deviceValue in pairs(devicechanged) do
+    for deviceName, deviceValue in pairs(heating_management) do
         commandArray['Gestion chauffage ' .. deviceName] = deviceValue
     end
 end
