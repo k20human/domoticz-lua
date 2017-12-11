b792a584e2e8538b2b6b2918439c83ad9a89d67c
Kévin Mathieu <kevin.mathieu@insidegroup.fr>
2017-12-11 09:35:42 +0100

    Set level

--- script_device_holiday.lua
+++ script_device_holiday.lua
@@ -2,21 +2,21 @@ commandArray = {}
 
 interupteur = 'Vacances'
 heating_management = {
-    ['chambre Théa'] = 'On',
-	['rdc'] = 'On',
-	['chambre parents'] = 'On',
-	['bureau'] = 'Off'
+    ['chambre Théa'] = '30',
+	['rdc'] = '30',
+	['chambre parents'] = '30',
+	['bureau'] = '0'
 }
 
 if devicechanged[interupteur] and otherdevices[interupteur] == 'On' then
     for deviceName, deviceValue in pairs(heating_management) do
         print ("Gestion " .. deviceName .. ", mode vacance");
-        commandArray['Gestion chauffage ' .. deviceName] = 'Vacances'
+        commandArray['Gestion chauffage ' .. deviceName] = 'Set Level: 20'
     end
 elseif devicechanged[interupteur] and otherdevices[interupteur] == 'Off' then
     for deviceName, deviceValue in pairs(heating_management) do
         print ("Gestion " .. deviceName .. ", " .. deviceValue);
-        commandArray['Gestion chauffage ' .. deviceName] = deviceValue
+        commandArray['Gestion chauffage ' .. deviceName] = 'Set Level: ' .. deviceValue
     end
 end
 
