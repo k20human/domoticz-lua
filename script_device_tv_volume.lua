6b0670d4dc86d2a58b2650b50953ae258adc65a1
k20human <kmat.null@gmail.com>
2017-12-29 17:40:23 +0100

    Add basic TV volume management

---
+++ script_device_tv_volume.lua
@@ -0,0 +1,17 @@
+-----------------------------------------
+-- Gestion du volume de la TV
+-----------------------------------------
+
+tvIp = '192.168.1.36'
+tvPort = '1925'
+volumeInterupteur = 'test'
+
+package.path = package.path .. ';' .. '/home/k20/domoticz/scripts/lua/?.lua'
+Library = require('Library')
+JSON = assert(loadfile "JSON.lua")()
+
+if devicechanged[volumeInterupteur] then
+    local volume = assert(io.popen('curl http://' .. tvIp .. ':' .. tvPort .. '/6/audio/volume'))
+    print(volume)
+    print(JSON:decode(volume))
+end
