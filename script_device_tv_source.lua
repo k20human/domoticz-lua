ea31441bba09b45356571303b60c470d98855fc4
Kévin Mathieu <kevin.mathieu@insidegroup.fr>
2018-01-02 15:17:40 +0100

    Try something

--- script_device_tv_source.lua
+++ script_device_tv_source.lua
@@ -14,7 +14,7 @@ package.path = package.path .. ';' .. '/home/k20/domoticz/scripts/lua/?.lua'
 Library = require('Library')
 
 if devicechanged[selector] then
-    Library.setTvKey(tvIp, tvPort, 'Source')
+  --  Library.setTvKey(tvIp, tvPort, 'Source')
 
     if (otherdevices[selector] == sourceShield) then
         Library.setTvKey(tvIp, tvPort, 'CursorDown')
@@ -24,21 +24,24 @@ if devicechanged[selector] then
         Library.setTvKey(tvIp, tvPort, 'CursorDown')
         Library.setTvKey(tvIp, tvPort, 'CursorDown')
     elseif (otherdevices[selector] == sourceTv) then
-        Library.setTvKey(tvIp, tvPort, 'CursorUp')
-        Library.sleep(0.1)
-        Library.setTvKey(tvIp, tvPort, 'CursorUp')
-        Library.sleep(0.1)
-        Library.setTvKey(tvIp, tvPort, 'CursorUp')
-        Library.sleep(0.1)
-        Library.setTvKey(tvIp, tvPort, 'CursorUp')
-        Library.sleep(0.1)
-        Library.setTvKey(tvIp, tvPort, 'CursorUp')
-        Library.sleep(0.1)
-        Library.setTvKey(tvIp, tvPort, 'CursorUp')
-        Library.sleep(0.1)
+    --    Library.setTvKey(tvIp, tvPort, 'CursorUp')
+    --    Library.sleep(0.1)
+    --    Library.setTvKey(tvIp, tvPort, 'CursorUp')
+    --    Library.sleep(0.1)
+    --    Library.setTvKey(tvIp, tvPort, 'CursorUp')
+    --    Library.sleep(0.1)
+    --    Library.setTvKey(tvIp, tvPort, 'CursorUp')
+    --    Library.sleep(0.1)
+    --    Library.setTvKey(tvIp, tvPort, 'CursorUp')
+    --    Library.sleep(0.1)
+    --    Library.setTvKey(tvIp, tvPort, 'CursorUp')
+    --    Library.sleep(0.1)
+        local runcommand = 'curl -X POST -H "Content-Type: application/json" -d \'{"key":"Source"}\' http://' .. tvIp .. ':' .. tvPort .. '/6/input/key'
+        runcommand = runcommand .. ' && curl -X POST -H "Content-Type: application/json" -d \'{"key":"CursorUp"}\' http://' .. tvIp .. ':' .. tvPort .. '/6/input/key'
+        os.execute(runcommand)
     end
 
-    Library.setTvKey(tvIp, tvPort, 'Confirm')
+  --  Library.setTvKey(tvIp, tvPort, 'Confirm')
 end
 
 return commandArray
