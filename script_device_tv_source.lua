d23baace4e32a5e99cd35a40032037c630b2e971
Kévin Mathieu <kevin.mathieu@insidegroup.fr>
2018-01-02 15:23:37 +0100

    Try something

--- script_device_tv_source.lua
+++ script_device_tv_source.lua
@@ -14,34 +14,30 @@ package.path = package.path .. ';' .. '/home/k20/domoticz/scripts/lua/?.lua'
 Library = require('Library')
 
 if devicechanged[selector] then
-  --  Library.setTvKey(tvIp, tvPort, 'Source')
+    if (otherdevices[selector] == sourceShield or otherdevices[selector] == sourceTv) then
+        Library.setTvKey(tvIp, tvPort, 'Source')
+        os.execute("sleep 1")
 
-    if (otherdevices[selector] == sourceShield) then
-        Library.setTvKey(tvIp, tvPort, 'CursorDown')
-        Library.setTvKey(tvIp, tvPort, 'CursorDown')
-        Library.setTvKey(tvIp, tvPort, 'CursorDown')
-        Library.setTvKey(tvIp, tvPort, 'CursorDown')
-        Library.setTvKey(tvIp, tvPort, 'CursorDown')
-        Library.setTvKey(tvIp, tvPort, 'CursorDown')
-    elseif (otherdevices[selector] == sourceTv) then
-    --    Library.setTvKey(tvIp, tvPort, 'CursorUp')
-    --    Library.sleep(0.1)
-    --    Library.setTvKey(tvIp, tvPort, 'CursorUp')
-    --    Library.sleep(0.1)
-    --    Library.setTvKey(tvIp, tvPort, 'CursorUp')
-    --    Library.sleep(0.1)
-    --    Library.setTvKey(tvIp, tvPort, 'CursorUp')
-    --    Library.sleep(0.1)
-    --    Library.setTvKey(tvIp, tvPort, 'CursorUp')
-    --    Library.sleep(0.1)
-    --    Library.setTvKey(tvIp, tvPort, 'CursorUp')
-    --    Library.sleep(0.1)
-        local runcommand = 'curl -X POST -H "Content-Type: application/json" -d \'{"key":"Source"}\' http://' .. tvIp .. ':' .. tvPort .. '/6/input/key'
-        runcommand = runcommand .. ' && curl -X POST -H "Content-Type: application/json" -d \'{"key":"CursorUp"}\' http://' .. tvIp .. ':' .. tvPort .. '/6/input/key'
-        os.execute(runcommand)
+        if (otherdevices[selector] == sourceShield) then
+            Library.setTvKey(tvIp, tvPort, 'CursorDown')
+            Library.setTvKey(tvIp, tvPort, 'CursorDown')
+            Library.setTvKey(tvIp, tvPort, 'CursorDown')
+            Library.setTvKey(tvIp, tvPort, 'CursorDown')
+            Library.setTvKey(tvIp, tvPort, 'CursorDown')
+            Library.setTvKey(tvIp, tvPort, 'CursorDown')
+        elseif (otherdevices[selector] == sourceTv) then
+            Library.setTvKey(tvIp, tvPort, 'CursorUp')
+            Library.setTvKey(tvIp, tvPort, 'CursorUp')
+            Library.setTvKey(tvIp, tvPort, 'CursorUp')
+            Library.setTvKey(tvIp, tvPort, 'CursorUp')
+            Library.setTvKey(tvIp, tvPort, 'CursorUp')
+            Library.setTvKey(tvIp, tvPort, 'CursorUp')
+        end
+
+        Library.setTvKey(tvIp, tvPort, 'Confirm')
     end
 
-  --  Library.setTvKey(tvIp, tvPort, 'Confirm')
+    commandArray[selector] = 'Set Level: 30'
 end
 
 return commandArray
