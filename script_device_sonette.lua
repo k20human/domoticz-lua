ad5b63cae5c24853e6d1dd073cc2e62a4944f0b6
Kévin Mathieu <kevin.mathieu@insidegroup.fr>
2018-01-13 13:54:49 +0100

    Add volume

--- script_device_sonette.lua
+++ script_device_sonette.lua
@@ -8,10 +8,12 @@ Library = require('Library')
 -- Variables
 local sonnette = 'Sonnette'
 local sonnetteSound = 'Sonnette son'
+local volume = 'Xiaomi Gateway Volume'
 
 commandArray = {}
 
 if (devicechanged[sonnette]) then
+	Library.addToCommand(volume, 'Set Level 15')
     Library.addToCommand(sonnetteSound, "Set Level: 10")
 	Library.addToCommand(sonnetteSound, "Set Level: 10 AFTER 2")
 end
