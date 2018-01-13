ae47f94b768943c5acb31a55c4ce030f7d10994b
Kévin Mathieu <kevin.mathieu@insidegroup.fr>
2018-01-13 13:40:04 +0100

    Modify TV start

---
+++ script_device_tv_on.lua
@@ -0,0 +1,30 @@
+-----------------------------------------
+-- Allumer la TV
+-----------------------------------------
+
+commandArray = {}
+
+tvIp = '192.168.1.36'
+tvPort = '1925'
+button = 'Allumer TV'
+buttonWol = 'Allumer TV wol'
+
+package.path = package.path .. ';' .. '/home/k20/domoticz/scripts/lua/?.lua'
+Library = require('Library')
+
+if (devicechanged[button]) then
+    local ping_success = os.execute('ping -c1 ' .. tvIp)
+
+    if ping_success then
+        print("TV - ping ok")
+        Library.setTvKey(tvIp, tvPort, 'Standby')
+    else
+        print("TV - ping fail - wol")
+        commandArray[buttonWol] = 'On'
+    end
+
+
+
+end
+
+return commandArray
