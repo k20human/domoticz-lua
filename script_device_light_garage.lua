bceabbf15e097597ae29bd92d56108cfdae42cc1
Kévin Mathieu <kevin.mathieu@insidegroup.fr>
2018-11-23 20:26:48 +0100

    Improve light garage (debug)

--- script_device_light_garage.lua
+++ script_device_light_garage.lua
@@ -7,18 +7,24 @@ lampes = 'Lampes garage'
 -- On ouvre la porte
 if devicechanged[interupteur] and devicechanged[interupteur] == 'Open' then
     commandArray['Group:' .. lampes] = 'On'
+    print('Garage - ouverture de porte allumage lumière')
 -- On ferme la porte
 elseif devicechanged[interupteur] and devicechanged[interupteur] ~= 'Open' then
     -- Si on ne détecte personne
-    if (otherdevices[detection] == 'Off')
+    if (otherdevices[detection] == 'Off') then
         commandArray['Group:' .. lampes] = 'Off'
+        print('Garage - fermeture de porte éteignage lumière')
+    else
+        print('Garage - fermeture de porte encore une personne')
     end
 -- Plus personne dans le garage
 elseif devicechanged[detection] and devicechanged[detection] == 'Off' then
     commandArray['Group:' .. lampes] = 'Off'
+    print('Garage - plus personne')
 -- Quelqu'un arrive dans le garage
 elseif devicechanged[detection] and devicechanged[detection] == 'On' then
     commandArray['Group:' .. lampes] = 'On'
+    print('Garage - une personne est là')
 end
 
 return commandArray
