64f9a0efc2e72cf59648030588172782fab845dd
kmathieu <kmat.null@gmail.com>
2018-12-31 18:02:41 +0100

    Add subwoffer

--- script_variable_good_night.lua
+++ script_variable_good_night.lua
@@ -4,14 +4,15 @@ package.path = package.path .. ';' .. '/home/k20/domoticz/scripts/lua/?.lua'
 Library = require('Library')
 
 variable = 'Bonne_Nuit'
-lampes = {
+appareils = {
     'Lampe salon',
     'Lampe cuisine',
-    'Lampe TV'
+    'Lampe TV',
+    'Interrupteur caisson'
 }
 
 if (uservariablechanged[variable] ~= nil) then
-    Library.onOffDevices(lampes, 'Off', '')
+    Library.onOffDevices(appareils, 'Off', '')
 end
 
 return commandArray
