443ff5f23eb387000c3383cc4f8e05714921e6ed
Kévin Mathieu <kevin.mathieu@insidegroup.fr>
2019-01-12 16:47:31 +0100

    Force light

--- script_device_light_garage.lua
+++ script_device_light_garage.lua
@@ -3,13 +3,14 @@ commandArray = {}
 interupteur = 'Porte garage'
 detection = 'Présence garage'
 lampes = 'Lampes garage'
+force = 'Forcer lumière garage'
 
 -- On ouvre la porte
 if devicechanged[interupteur] and devicechanged[interupteur] == 'Open' then
     commandArray['Group:' .. lampes] = 'On'
     print('Garage - ouverture de porte allumage lumière')
 -- On ferme la porte
-elseif devicechanged[interupteur] and devicechanged[interupteur] ~= 'Open' then
+elseif devicechanged[interupteur] and devicechanged[interupteur] ~= 'Open' and otherdevices[force] == 'Off' then
     -- Si on ne détecte personne
     if (otherdevices[detection] == 'Off') then
         commandArray['Group:' .. lampes] = 'Off'
@@ -18,7 +19,7 @@ elseif devicechanged[interupteur] and devicechanged[interupteur] ~= 'Open' then
         print('Garage - fermeture de porte encore une personne')
     end
 -- Plus personne dans le garage
-elseif devicechanged[detection] and devicechanged[detection] == 'Off' then
+elseif devicechanged[detection] and devicechanged[detection] == 'Off' and otherdevices[force] == 'Off' then
     commandArray['Group:' .. lampes] = 'Off'
     print('Garage - plus personne')
 -- Quelqu'un arrive dans le garage
