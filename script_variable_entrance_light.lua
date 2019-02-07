1126ec673870cd344c77277ac7a445e7bb9a70c1
Kévin Mathieu <kevin.mathieu@insidegroup.fr>
2019-02-07 12:36:37 +0100

    Update entrance light management

--- script_variable_entrance_light.lua
+++ script_variable_entrance_light.lua
@@ -8,6 +8,7 @@ Library = require('Library')
 -- Variables
 local nut = 'Nut_Kevin'
 local lampe = 'Lampe entrée'
+local luminosite = 'Luminosité entrée'
 local days = {
 	1,
 	2,
@@ -23,7 +24,7 @@ if (uservariablechanged[nut] ~= nil) then
 	local heure = Library.getCurrentHours()
 	local day = Library.getDayNumberOfWeek()
 
-	if (uservariables[nut] ~= "AWAY") and (heure == 18) and (minutes >= 15) and (minutes <= 50) and (Library.tableContains(days, day)) then
+	if (uservariables[nut] ~= "AWAY") and (heure == 18) and (minutes >= 15) and (minutes <= 50) and (Library.tableContains(days, day)) and tonumber(otherdevices[luminosite]) < 10 then
 		commandArray[lampe] = 'On'
 	end
 end
