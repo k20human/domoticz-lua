88a61d0deddea478bde934d15c9cd5dc35db5671
Kévin Mathieu <kevin.mathieu@insidegroup.fr>
2019-06-16 19:39:04 +0200

    Add level and off

--- script_device_heart.lua
+++ script_device_heart.lua
@@ -24,19 +24,20 @@ if devicechanged[heartLeft] or devicechanged[heartRight] then
         deviceName = heartRight
     end
 
-    if otherdevices[deviceName] == 'Off' then
+    local acolor = " "
+    local loadData = 'curl "http://192.168.1.14:8080/json.htm?type=devices&rid=' .. idx .. '"'
+    local jsondata = assert(io.popen(loadData))
+    local jsondevices = jsondata:read('*all')
+    jsondata:close()
+    local jsonCPM = Json:decode(jsondevices)
+    local acolor = jsonCPM.result[1].Color
+    local level = jsonCPM.result[1].Level
+    local status = jsonCPM.result[1].Status
+
+    if status == 'Off' then
         local runcommand = 'curl -X GET  "http://' .. ip .. ':80?side='  .. side .. '&stop=1'
         os.execute(runcommand)
     else
-        local acolor = " "
-        local loadData = 'curl "http://192.168.1.14:8080/json.htm?type=devices&rid=' .. idx .. '"'
-        local jsondata = assert(io.popen(loadData))
-        local jsondevices = jsondata:read('*all')
-        jsondata:close()
-        local jsonCPM = Json:decode(jsondevices)
-        local acolor = jsonCPM.result[1].Color
-        local level = jsonCPM.result[1].Level
-
         acolorj = acolor:gsub("}",",}")
         for k,v in acolorj:gmatch('"(.-)":(.-),') do
             if (k == 'b') then
