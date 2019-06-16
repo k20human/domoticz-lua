c822210c56cd978a7c42b6f27605297989cb58c3
Kévin Mathieu <kevin.mathieu@insidegroup.fr>
2019-06-16 19:32:21 +0200

    Add level and off

--- script_device_heart.lua
+++ script_device_heart.lua
@@ -12,36 +12,45 @@ ip = '192.168.1.93'
 if devicechanged[heartLeft] or devicechanged[heartRight] then
     local side = ''
     local idx = 0
+    local deviceName = ''
 
     if (devicechanged[heartLeft]) then
         side = 'left'
         idx = heartLeftId
+        deviceName = heartLeft
     else
         side = 'right'
         idx = heartRightId
-    end
-    
-    local acolor = " "
-    local loadData = 'curl "http://192.168.1.14:8080/json.htm?type=devices&rid=' .. idx .. '"'
-    local jsondata = assert(io.popen(loadData))
-    local jsondevices = jsondata:read('*all')
-    jsondata:close()
-    local jsonCPM = Json:decode(jsondevices)
-    local acolor = jsonCPM.result[1].Color
-    
-    acolorj = acolor:gsub("}",",}")
-    for k,v in acolorj:gmatch('"(.-)":(.-),') do
-        if (k == 'b') then
-            blue = v
-        elseif (k == 'r') then
-            red = v
-        elseif (k == 'g') then
-            green = v
-        end
+        deviceName = heartRight
     end
 
-    local runcommand = 'curl -X GET  "http://' .. ip .. ':80?side='  .. side .. '&r=' .. red .. '&g=' .. green .. '&b=' .. blue .. '"'
-    os.execute(runcommand)
+    if otherdevices[deviceName] == 'Off' then
+        local runcommand = 'curl -X GET  "http://' .. ip .. ':80?side='  .. side .. '&stop=1'
+        os.execute(runcommand)
+    else
+        local acolor = " "
+        local loadData = 'curl "http://192.168.1.14:8080/json.htm?type=devices&rid=' .. idx .. '"'
+        local jsondata = assert(io.popen(loadData))
+        local jsondevices = jsondata:read('*all')
+        jsondata:close()
+        local jsonCPM = Json:decode(jsondevices)
+        local acolor = jsonCPM.result[1].Color
+        local level = jsonCPM.result[1].Level
+
+        acolorj = acolor:gsub("}",",}")
+        for k,v in acolorj:gmatch('"(.-)":(.-),') do
+            if (k == 'b') then
+                blue = v
+            elseif (k == 'r') then
+                red = v
+            elseif (k == 'g') then
+                green = v
+            end
+        end
+
+        local runcommand = 'curl -X GET  "http://' .. ip .. ':80?side='  .. side .. '&r=' .. red .. '&g=' .. green .. '&b=' .. blue .. '&l=' .. level .. '"'
+        os.execute(runcommand)
+    end
 end
 
 return commandArray
