1091083fa0db73e0caf93ccd2cda3e56578f1b23
k20human <kmat.null@gmail.com>
2019-09-21 14:16:36 +0200

    Add new garage detector

--- script_device_light_garage.lua
+++ script_device_light_garage.lua
@@ -2,6 +2,7 @@ commandArray = {}
 
 interupteur = 'Porte garage'
 detection = 'Présence garage'
+detection2 = 'Présence garage 2'
 lampes = 'Lampes garage'
 force = 'Forcer lumière garage'
 
@@ -12,18 +13,18 @@ if devicechanged[interupteur] and devicechanged[interupteur] == 'Open' then
 -- On ferme la porte
 elseif devicechanged[interupteur] and devicechanged[interupteur] ~= 'Open' and otherdevices[force] == 'Off' then
     -- Si on ne détecte personne
-    if (otherdevices[detection] == 'Off') then
+    if (otherdevices[detection] == 'Off' and otherdevices[detection2] == 'Off') then
         commandArray['Group:' .. lampes] = 'Off'
         print('Garage - fermeture de porte éteignage lumière')
     else
         print('Garage - fermeture de porte encore une personne')
     end
 -- Plus personne dans le garage
-elseif devicechanged[detection] and devicechanged[detection] == 'Off' and otherdevices[force] == 'Off' then
+elseif (devicechanged[detection] or devicechanged[detection2]) and devicechanged[detection] == 'Off' and devicechanged[detection2] == 'Off' and otherdevices[force] == 'Off' then
     commandArray['Group:' .. lampes] = 'Off'
     print('Garage - plus personne')
 -- Quelqu'un arrive dans le garage
-elseif devicechanged[detection] and devicechanged[detection] == 'On' then
+elseif (devicechanged[detection] or devicechanged[detection2]) and (devicechanged[detection] == 'On' or devicechanged[detection2] == 'On') then
     commandArray['Group:' .. lampes] = 'On'
     print('Garage - une personne est là')
 end
